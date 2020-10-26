b5d3ae59ef84dda40c0ff04fee5b3342dbc83ff0
jtw1 <47053853+jtw1@users.noreply.github.com>
2020-10-26 16:06:11 +0800

    Create Solu1365.java

---
+++ code/Array/Solu1365.java
@@ -0,0 +1,46 @@
+// 1365 小于当前数字的数字的数目
+// https://leetcode-cn.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+//https://leetcode-cn.com/problems/how-many-numbers-are-smaller-than-the-current-number/solution/you-duo-shao-xiao-yu-dang-qian-shu-zi-de-shu-zi--2/
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        // TC:O(nlogn)  SC:O(n)
+        int n = nums.length;
+        int[][] data = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            data[i][0] = nums[i];
+            data[i][1] = i;
+        }
+        Arrays.sort(data, new Comparator<int[]>() {
+            public int compare(int[] data1, int[] data2) {
+                return data1[0] - data2[0];
+            }
+        });
+
+        int[] ret = new int[n];
+        int prev = -1;
+        for (int i = 0; i < n; i++) {
+            if (prev == -1 || data[i][0] != data[i - 1][0]) {
+                prev = i;
+            }
+            ret[data[i][1]] = prev;
+        }
+        return ret;
+        
+        // TC:O(N+K)  SC:O(N)
+        int[] cnt = new int[101];
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            cnt[nums[i]]++;
+        }
+        for (int i = 1; i <= 100; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+        int[] ret = new int[n];
+        for (int i = 0; i < n; i++) {
+            ret[i] = nums[i] == 0 ? 0 : cnt[nums[i] - 1];
+        }
+        return ret;
+
+    }
+}
