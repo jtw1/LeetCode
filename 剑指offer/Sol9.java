34b18ac889cd0eb987bbb5e14608b40aea7438c7
jtwOfLab <1481682673@qq.com>
2020-12-09 11:27:25 +0800

    剑指offer 9

---
+++ 剑指offer/Sol9.java
@@ -0,0 +1,47 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @Description 面试题9 用两个栈实现一个队列 P69
+ * @date 2020/12/9 0009-10:57
+ */
+class CQueue {
+    private Deque<Integer> stack1;
+    private Deque<Integer> stack2;
+    public CQueue() {
+        stack1=new LinkedList<Integer>();
+        stack2=new LinkedList<Integer>();
+    }
+
+    /**
+     * 队列：FIFO
+     * 入队列
+     * @param value
+     */
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    /**
+     * 出队列
+     * @return 删除的队列元素
+     */
+    public int deleteHead() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                int temp=stack1.pop();
+                stack2.push(temp);
+            }
+        }
+        if(stack2.isEmpty()){
+            return -1;
+        }else{
+            int head=stack2.pop();
+            return head;
+        }
+    }
+}
+
+//public class Sol9 {
+//
+//}
