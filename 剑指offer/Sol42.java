65558cd0e8afe8aa90ed60c2685aa52ea21e3391
jtwOfLab <1481682673@qq.com>
2021-01-25 14:30:53 +0800

    Point to offer

---
+++ 剑指offer/Sol42.java
@@ -0,0 +1,34 @@
+/**
+ * @Description 42 连续子数组的最大和
+ * https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+ * @date 2021/1/25 0025-14:28
+ */
+public class Sol42 {
+    //TC:O(n) SC:O(1)
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i-1] > 0) {
+                nums[i] = nums[i-1]+nums[i];
+            }
+            max = (nums[i]>max? nums[i]:max);
+        }
+        return max;
+    }
+    //TC:O(n) SC:O(n)
+    public int maxSubArray1(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = nums[0];
+
+        for (int i = 1; i < dp.length; i++) {
+            if (dp[i-1] > 0) {
+                dp[i] = dp[i-1]+nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
+            max = (dp[i]>max? dp[i]:max);
+        }
+        return max;
+    }
+}
