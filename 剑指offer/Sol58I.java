08d8dd17412ab04e1bd5b89c8bcea41fe43bddeb
jtwPC <1481682673@qq.com>
2021-02-11 16:27:50 +0800

    point to offer

---
+++ 剑指offer/Sol58I.java
@@ -0,0 +1,30 @@
+/**
+ * @Description 58-I 翻转单词顺序
+ * https://leetcode-cn.com/problems/fan-zhuan-dan-ci-shun-xu-lcof/
+ * @date 2021/2/11-14:35
+ */
+public class Sol58I {
+    public String reverseWords(String s) {
+        //内置函数
+        String[] strs = s.trim().split(" "); // 删除首尾空格，分割字符串
+        StringBuilder res = new StringBuilder();
+        for(int i = strs.length - 1; i >= 0; i--) { // 倒序遍历单词列表
+            if(strs[i].equals("")) continue; // 遇到空单词则跳过
+            res.append(strs[i] + " "); // 将单词拼接至 StringBuilder
+        }
+        return res.toString().trim(); // 转化为字符串，删除尾部空格，并返回
+    }
+    //双指针
+    public String reverseWords1(String s) {
+        s=s.trim();
+        int j=s.length()-1,i=j;
+        StringBuilder res = new StringBuilder();
+        while(i>=0){
+            while(i>=0 && s.charAt(i)!=' ') i--;  //搜索首个空格
+            res.append(s.substring(i+1,j+1)+" ");
+            while(i >= 0 && s.charAt(i) == ' ') i--; // 跳过单词间空格
+            j=i;
+        }
+        return res.toString().trim();
+    }
+    }
