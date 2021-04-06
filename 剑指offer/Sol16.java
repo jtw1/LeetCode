32a697dca75389ed638b8d6fab275383b6f3c580
jtwOfLab <1481682673@qq.com>
2021-04-06 15:27:36 +0800

    剑指offer

--- 剑指offer/Sol16.java
+++ 剑指offer/Sol16.java
@@ -4,8 +4,13 @@
  * @date 2021/1/9-14:02
  */
 public class Sol16 {
+    public static void main(String[] args) {
+        Sol16 test=new Sol16();
+        double res=test.myPow(2.00000 ,-2147483648);
+        System.out.println(res);
+    }
     //TC:O(logn) SC:O(1)
-    public double myPow(double x, int n) {
+    public double myPow1(double x, int n) {
         if(x==0) return 0;
         long b=n;
         double res=1.0;
@@ -20,4 +25,26 @@ public class Sol16 {
         }
         return res;
     }
+
+
+    public double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }else if (n<0){
+            return 1/(x*myPow(x,-n-1));
+        }else if (n%2==1){
+            return x*myPow(x,n-1);
+        }else{
+            return myPow(x,n>>1);
+        }
+    }
+//    private double powWithUnsignedExponent(double x, long n){
+//        if(n==0) return 1.0;
+//        if(n==1) return x;
+//        if(Double.doubleToLongBits(x) == Double.doubleToLongBits(1.00000)) return x;
+//        double result=powWithUnsignedExponent(x,n>>1);
+//        result *= result;
+//        if((n&1)==1) result *= x;
+//        return result;
+//    }
 }
